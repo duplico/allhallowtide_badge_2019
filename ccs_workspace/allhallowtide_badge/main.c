416ae7a5b5d25a4135c8e2c9c179a6a6c0e3d073
George Louthan <duplico@dupli.co>
2019-10-18 22:18:06 -0500

    Integrate with CapTIvate files, and up MCLK to 16 MHz.

--- ccs_workspace/allhallowtide_badge/main.c
+++ ccs_workspace/allhallowtide_badge/main.c
@@ -1,11 +1,13 @@
 #include "driverlib.h"
 #include <msp430fr2512.h>
-#include <tlc5948a.h>
+
+#include "tlc5948a.h"
+#include "CAPT_App.h"
 
 /// Initialize clock signals and the three system clocks.
 /**
- ** We'll take the DCO to 8 MHz, and divide it by 1 for MCLK.
- ** Then we'll divide MCLK by 1 to get 8 MHz SMCLK.
+ ** We'll take the DCO to 16 MHz, and divide it by 1 for MCLK.
+ ** Then we'll divide MCLK by 8 to get 2 MHz SMCLK.
  **
  ** Our available clock sources are:
  **  VLO:     10kHz very low power low-freq
@@ -23,15 +25,17 @@
  */
 void init_clocks() {
     // DCO  (Digitally-controlled oscillator)
-    //  Let's bring this up to 8 MHz or so.
+    //  Let's bring this up to 16 MHz or so.
 
     __bis_SR_register(SCG0);                // disable FLL
     CSCTL3 |= SELREF__REFOCLK;              // Set REFO as FLL reference source
     CSCTL0 = 0;                             // clear DCO and MOD registers
     CSCTL1 &= ~(DCORSEL_7);                 // Clear DCO frequency select bits first
-    CSCTL1 |= DCORSEL_3;                    // Set DCO = 8MHz
+    CSCTL1 |= DCORSEL_5;                    // Set DCO = 16MHz
+
     // CSCTL feedback loop:
-    CSCTL2 = FLLD_0 + 243;                  // DCODIV = /1
+    // (f_output = (FLLN + 1) * (322768 Hz / outputDiv)
+    CSCTL2 = FLLD_0 + 487;                  // DCODIV = /1
     __delay_cycles(3);
     __bic_SR_register(SCG0);                // enable FLL
     while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1)); // Poll until FLL is locked
@@ -39,15 +43,17 @@ void init_clocks() {
     // SYSTEM CLOCKS
     // =============
 
-    // MCLK (8 MHz)
+    // MCLK (16 MHz)
     //  All sources but MODOSC are available at up to /128
-    //  Set to DCO/1 = 8 MHz
-    CSCTL5 |= DIVM__1;
+    //  Set to DCO/1 = 16 MHz
+    // DIVM__1;
 
     // SMCLK (8 MHz)
     //  Derived from MCLK with divider up to /8
-    //  Set to MCLK/1 = 8 MHz
-    CSCTL5 |= DIVS__1;
+    //  Set to MCLK/8 = 2 MHz
+    // DIVS__8;
+
+    CSCTL5 = VLOAUTOOFF_1 | DIVS__8 | DIVM__1;
 }
 
 /// Apply the initial configuration of the GPIO and peripheral pins.
@@ -55,9 +61,8 @@ void init_clocks() {
  **
  */
 void init_io() {
-    // Unlock the pins from high-impedance mode:
-    // (AKA the MSP430FR magic make-it-work command)
-    PM5CTL0 &= ~LOCKLPM5;
+    // TODO:
+    // USCIARMP=1
 
     // GPIO:
     // P1.0     LAT         (SEL 00; DIR 1)
@@ -73,7 +78,7 @@ void init_io() {
     P1SEL1 = 0b00001000; // MSB
     P1REN = 0b00100000;
     P1OUT = 0b00100000;
-
+    // TODO: P2.2: SYNC (SET OUTPUT LOW UNLESS USED)
     // P2.0     UCA0TXD     (SEL 01; DIR 1)
     // P2.1     UCA0RXD     (SEL 01; DIR 0)
     // P2.2     unused      (SEL 00; DIR 0)
@@ -82,6 +87,10 @@ void init_io() {
     P2SEL1 = 0b000;
     P2REN = 0b000;
     P2OUT = 0b000;
+
+    // Unlock the pins from high-impedance mode:
+    // (AKA the MSP430FR magic make-it-work command)
+    PM5CTL0 &= ~LOCKLPM5;
 }
 
 void write_conf() {
@@ -108,6 +117,10 @@ int main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;
 
+    // TODO:
+    // Configure FRAM wait state (set to 1 to support 16MHz MCLK)
+    FRAMCtl_configureWaitStateControl(FRAMCTL_ACCESS_TIME_CYCLES_1);
+
     init_clocks();
     init_io();
 
@@ -115,7 +128,32 @@ int main(void) {
 
     __bis_SR_register(GIE);
 
-    while (1) {
+    CAPT_appStart();
 
-    }
+    while(1)
+    {
+        //
+        // Run the captivate application handler.
+        //
+        CAPT_appHandler();
+
+        //
+        // This is a great place to add in any
+        // background application code.
+        //
+        __no_operation();
+
+        //
+        // End of background loop iteration
+        // Go to sleep if there is nothing left to do
+        //
+        CAPT_appSleep();
+
+    } // End background loop
+}
+
+int _system_pre_init(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+    return 1;
 }
