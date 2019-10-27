5fd16295533cc65051abd198b2a96d926f5704b1
George Louthan <duplico@dupli.co>
2019-10-26 19:33:51 -0500

    Add a boop animation.

--- head_anims/band_anims.h
+++ head_anims/band_anims.h
@@ -28,6 +28,12 @@ extern uint16_t geek_girl_band_fade_durs[];
 extern const band_animation_t geek_girl_band;
 extern const band_animation_t *geek_girl_anim_set[1];
 // frames for the band:
+extern const rgbcolor_t meta_boop_band_frames[][4];
+extern uint16_t meta_boop_band_durations[];
+extern uint16_t meta_boop_band_fade_durs[];
+extern const band_animation_t meta_boop_band;
+extern const band_animation_t *meta_boop_anim_set[1];
+// frames for the band:
 extern const rgbcolor_t rainbow_band_frames[][4];
 extern uint16_t rainbow_band_durations[];
 extern uint16_t rainbow_band_fade_durs[];
@@ -58,7 +64,7 @@ extern uint16_t zflag_trans_band_fade_durs[];
 extern const band_animation_t zflag_trans_band;
 extern const band_animation_t *zflag_trans_anim_set[1];
 #define HEAD_ANIM_COUNT 8
-#define HEAD_ANIM_COUNT_INCL_META 8
+#define HEAD_ANIM_COUNT_INCL_META 9
 #define HEAD_ANIM_DEF 0
 #define HEAD_ANIM_FIRE 1
 #define HEAD_ANIM_GEEK_GIRL 2
@@ -67,11 +73,11 @@ extern const band_animation_t *zflag_trans_anim_set[1];
 #define HEAD_ANIM_ZFLAG_BI 5
 #define HEAD_ANIM_ZFLAG_LEATHER 6
 #define HEAD_ANIM_ZFLAG_TRANS 7
-#define ANIM_TYPE_SLOWTWINKLE 0
+#define HEAD_ANIM_META_BOOP 8
+#define ANIM_TYPE_SOLID 0
 #define ANIM_TYPE_FASTTWINKLE 1
-#define ANIM_TYPE_HARDTWINKLE 2
-#define ANIM_TYPE_SOLID 3
-#define LEG_ANIM_TYPE_COUNT 4
+#define ANIM_TYPE_SLOWTWINKLE 2
+#define LEG_ANIM_TYPE_COUNT 3
 extern const band_animation_t **legs_all_anim_sets[];
 #endif // _H_
 extern const rgbcolor_t sprays[];
