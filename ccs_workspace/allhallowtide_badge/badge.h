c63b93186fff7f959581d4b5ac552d61edde34b9
George Louthan <duplico@dupli.co>
2019-10-28 02:18:17 -0500

    Add an untested remote boop feature.

--- ccs_workspace/allhallowtide_badge/badge.h
+++ ccs_workspace/allhallowtide_badge/badge.h
@@ -8,6 +8,8 @@
 #ifndef BADGE_H_
 #define BADGE_H_
 
+#define REMOTE_BOOP_MSG 0xf1
+
 typedef struct {
     uint8_t badge_id;
     uint32_t badges_seen;
@@ -16,6 +18,8 @@ typedef struct {
 } badge_conf_t;
 
 extern volatile uint8_t f_paired;
+// TODO: test:
+extern volatile uint8_t f_remote_boop;
 extern badge_conf_t badge_conf;
 
 #endif /* BADGE_H_ */
